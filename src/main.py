852a8dd77078500dcb72590fd00fc49cc6baf044
Maciej Krzyżowski <maciej.krzyzowski9@gmail.com>
2025-11-23 14:01:54 +0100

    zadanie 2 (#86c6nyp1q)

--- src/main.py
+++ src/main.py
@@ -3,6 +3,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
+class Movie:
+    def __init__(self, movieId, title, genres):
+        self.movieId = movieId
+        self.title = title
+        self.genres = genres
+
+    def __repr__(self):
+        return f"Movie({self.movieId}: '{self.title}' [{self.genres}])"
+
+def load_movies():
+    print("Opening movies.csv...")
+    with open('./movies/movies.csv', 'r') as f:
+        for (idx, line) in enumerate(f.readlines()):
+            if idx == 0: continue
+            movie = line.strip().split(',')
+            movieId = movie[0]
+            title = movie[1]
+            genres = movie[2]
+            yield Movie(movieId, title, genres)
+
 @app.route("/")
 def hello_world():
     return {'hello': 'world'}
+
+@app.route("/movies/")
+def get_movies():
+    movies = [ movie.__dict__ for movie in load_movies() ]
+    return movies
